021b871a2bf3691d4d6c21798c7690a718c6346b
jeongjaino <akgk155@gmail.com>
2023-08-20 14:56:33 +0900

    [FEATURE] #5 : MainActivity 네비게이션 구현 및 리소스 추가

--- android/app/src/main/java/com/jaino/napped/main/MainActivity.kt
+++ android/app/src/main/java/com/jaino/napped/main/MainActivity.kt
@@ -4,22 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jaino.napped.R
+import com.jaino.napped.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         initNavController()
     }
 
     private fun initNavController(){
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
-
         navController = navHostFragment.navController
+        binding.mainBottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
