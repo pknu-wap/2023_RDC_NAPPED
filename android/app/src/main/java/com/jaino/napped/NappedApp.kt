5ece4a00b6da10365c1cf88efac11104ac914c68
jeongjaino <akgk155@gmail.com>
2023-08-20 15:08:54 +0900

    [CHORE] #7 : Timber set up

---
+++ android/app/src/main/java/com/jaino/napped/NappedApp.kt
@@ -0,0 +1,13 @@
+package com.jaino.napped
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class NappedApp: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
