718eaca78f9c3f082626d739e3ff6307bca87189
jeongjaino <akgk155@gmail.com>
2023-08-23 17:06:22 +0900

    [FEATURE] #10 : Paging common, ksp 종속성 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("com.google.firebase.crashlytics") version "2.9.8" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
     kotlin("jvm") version "1.8.20" apply false
     kotlin("plugin.serialization") version "1.9.0" apply false
 }
