086d55dcc7314a205906446b8dd568730f00aec6
jeongjaino <akgk155@gmail.com>
2023-08-23 17:25:31 +0900

    [FEATURE] #10 : 채용 상세 페이지 구현

--- android/app/src/main/java/com/jaino/napped/information/InformationFragment.kt
+++ android/app/src/main/java/com/jaino/napped/information/InformationFragment.kt
@@ -1,4 +1,4 @@
-package com.jaino.napped.employment
+package com.jaino.napped.information
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,26 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.jaino.napped.R
 import com.jaino.napped.databinding.FragmentInformationBinding
+import com.jaino.napped.model.UiEvent
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
+@AndroidEntryPoint
 class InformationFragment: Fragment() {
     private var _binding: FragmentInformationBinding? = null
     private val binding get() = requireNotNull(_binding) { "binding object is not initialized" }
+
+    private val viewModel: InformationViewModel by viewModels()
+    private val args: InformationFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,12 +35,15 @@ class InformationFragment: Fragment() {
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_information, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initButton()
+        initData()
+        observeData()
     }
 
     private fun initButton(){
@@ -37,12 +53,31 @@ class InformationFragment: Fragment() {
         }
 
         binding.btInformationScrap.setOnClickListener{
-            Toast.makeText(requireContext(), "스크랩 되었습니다.", Toast.LENGTH_SHORT).show()
+            viewModel.addFavorite()
         }
 
         binding.btInformationApply.setOnClickListener {
             Toast.makeText(requireContext(), "지원에 성공하였습니다.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun initData(){
+        viewModel.getEmploymentInformation(args.employmentId)
+    }
+
+    private fun observeData(){
+        viewModel.informationUiEvent.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when(it){
+                    is UiEvent.Success -> {
+                        Toast.makeText(requireContext(), "관심 목록에 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                    is UiEvent.Failure -> {
+                        Timber.d(it.error)
+                    }
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     override fun onDestroy() {
